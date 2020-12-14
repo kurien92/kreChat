9bc2718f2f4fe4dfeeb3a8aebef368d913a171fe
Kurien <kurien92@gmail.com>
2020-12-14 21:09:33 +0900

    - Socket Server와 Socket Client 연결 프로그램 제작

---
+++ src/net/kurien/chat/SocketClient.java
@@ -0,0 +1,34 @@
+package net.kurien.chat;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class SocketClient {
+    private static final String SERVER_IP = "127.0.0.1";
+    private static final int SERVER_PORT = 5000;
+
+    public static void main(String[] args) {
+        Socket socket = null;
+
+        try {
+            // 1. 서버에 연결할 소켓을 생성한다.
+            socket = new Socket();
+
+            // 2. 연결될 서버의 정보를 입력한 후 연결한다.
+            socket.connect(new InetSocketAddress(SERVER_IP, SERVER_PORT));
+
+            // 3. 더 이상 진행할 작업이 없으므로 프로그램이 종료된다.
+        } catch(IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(socket != null && !socket.isClosed()) {
+                    socket.close();
+                }
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/net/kurien/chat/SocketServer.java
@@ -0,0 +1,48 @@
+package net.kurien.chat;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SocketServer {
+    public static void main(String[] args) {
+	    final int SERVER_PORT = 5000;
+
+        ServerSocket serverSocket = null;
+
+        try {
+            // 1.클라이언트에게 서비스를 제공할 서버 소켓을 생성한다.
+            serverSocket = new ServerSocket();
+
+            String serverAddress = InetAddress.getLocalHost().getHostAddress();
+            // 2. 서버 소켓에 접근할 수 있는 IP와 Port를 연결시켜준다.
+            serverSocket.bind(new InetSocketAddress(serverAddress, SERVER_PORT));
+
+            System.out.println("serverSocket.bind - serverAddress:" + serverAddress + ", serverPort:" + SERVER_PORT);
+
+            // 3. 클라이언트가 연결될 때까지 대기하고, 클라이언트가 연결되면 클라이언트와 연결된 socket을 생성한다.
+            Socket socket = serverSocket.accept();
+
+            // 4. 클라이언트의 IP와 Port 정보를 가져온다.
+            InetSocketAddress remoteSocketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();
+            String remoteAddress = remoteSocketAddress.getAddress().getHostAddress();
+            int remotePort = remoteSocketAddress.getPort();
+
+            System.out.println("serverSocket.accept -\n remoteAddress:" + remoteAddress + ", remotePort:" + remotePort);
+
+            // 5. 더 이상 진행할 작업이 없으므로 프로그램이 종료된다.
+        } catch(IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(serverSocket != null && !serverSocket.isClosed()) {
+                    serverSocket.close();
+                }
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
